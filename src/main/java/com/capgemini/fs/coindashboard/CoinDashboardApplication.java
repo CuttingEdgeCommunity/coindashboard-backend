b557db79cc4d1831afdbb6786ce5e8b78f566f67
Przemysław Kobylański <kobylanski.przemyslaw@gmail.com>
2022-06-24 16:07:19 +0200

    init

---
+++ src/main/java/com/capgemini/fs/coindashboard/CoinDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.fs.coindashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinDashboardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CoinDashboardApplication.class, args);
+  }
+}
