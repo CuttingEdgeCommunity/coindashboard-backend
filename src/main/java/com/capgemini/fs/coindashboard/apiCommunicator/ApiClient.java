6c22296f97f89328c3d733ed76e581939954059b
jmakarew <jakub.makarewicz@capgemini.com>
2022-08-02 11:25:57 +0200

    json parser

--- src/main/java/com/capgemini/fs/coindashboard/apiCommunicator/ApiClient.java
+++ src/main/java/com/capgemini/fs/coindashboard/apiCommunicator/ApiClient.java
@@ -1,6 +1,9 @@
 package com.capgemini.fs.coindashboard.apiCommunicator;
 
 import com.capgemini.fs.coindashboard.apiCommunicator.utils.RequestBuilder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -36,5 +39,10 @@ public class ApiClient {
       .buildURLConnectionGET(uri)
       .getInputStream();
     return this.convertStreamToString(response);
+  }
+
+  public JsonNode parseResponse(String json) throws JsonProcessingException { // TODO: Move outside of this class
+    ObjectMapper mapper = new ObjectMapper();
+    return mapper.readTree(json);
   }
 }
