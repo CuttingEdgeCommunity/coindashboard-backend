8c37d81b9b7f76a9e610e6697f3acd02ebd756af
jmakarew <jakub.makarewicz@capgemini.com>
2022-08-03 17:18:03 +0200

    implementation of ApiHolder implemented getCoinMarketData

--- src/main/java/com/capgemini/fs/coindashboard/apiCommunicator/ApiHolder.java
+++ src/main/java/com/capgemini/fs/coindashboard/apiCommunicator/ApiHolder.java
@@ -1,7 +1,13 @@
 package com.capgemini.fs.coindashboard.apiCommunicator;
 
+import com.capgemini.fs.coindashboard.apiCommunicator.dtos.common.Result;
+import com.capgemini.fs.coindashboard.apiCommunicator.dtos.common.ResultStatus;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,4 +28,23 @@ public class ApiHolder { // TODO: placeholder name
     return this.apiCommunicators.get(apiProviderEnum);
   }
 
+  public List<Result> getCoinMarketData(String coinName) {
+    List<Result> results = new ArrayList<>();
+    Iterator<Entry<ApiProviderEnum, ApiCommunicator>> iterator = this.apiCommunicators.entrySet()
+        .iterator();
+    boolean stop = false;
+    while (iterator.hasNext() && !stop) {
+      Map.Entry<ApiProviderEnum, ApiCommunicator> entry = iterator.next();
+      Result result = entry.getValue().getCurrentListing(new ArrayList<>() {{
+        add(coinName);
+      }}, new ArrayList<>() {{
+        add("usd");
+      }});
+      results.add(result);
+      if (result.getStatus() == ResultStatus.SUCCESS) {
+        stop = true;
+      }
+    }
+    return results;
+  }
 }
--- src/main/java/com/capgemini/fs/coindashboard/apiCommunicator/dtos/common/Result.java
+++ src/main/java/com/capgemini/fs/coindashboard/apiCommunicator/dtos/common/Result.java
@@ -1,17 +1,20 @@
 package com.capgemini.fs.coindashboard.apiCommunicator.dtos.common;
 
+import com.capgemini.fs.coindashboard.apiCommunicator.ApiProviderEnum;
 import lombok.Data;
 
 @Data
 public class Result {
 
+  private ApiProviderEnum provider;
   private ResultStatus status;
   private String errorMessage;
 
   public Result() {
   }
 
-  public Result(ResultStatus status, String errorMessage) {
+  public Result(ApiProviderEnum provider, ResultStatus status, String errorMessage) {
+    this.provider = provider;
     this.status = status;
     this.errorMessage = errorMessage;
   }
