f7fa395db417809c46989ddb8c809f87983e1d74
jmakarew <jakub.makarewicz@capgemini.com>
2022-08-05 11:54:22 +0200

    Testing added useless DTO tests

---
+++ src/test/java/com/capgemini/fs/coindashboard/apiCommunicator/dtos/marketData/PriceDtoTest.java
@@ -0,0 +1,30 @@
+package com.capgemini.fs.coindashboard.apiCommunicator.dtos.marketData;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+class PriceDtoTest {
+
+  private final PriceDto priceA = new PriceDto(123d, 123L);
+  private final PriceDto priceB = new PriceDto(13d, 123L);
+  private final PriceDto priceC = new PriceDto(123d, 123L);
+
+  @Test
+  void testEquals() {
+    assertEquals(this.priceA, this.priceC);
+    assertNotEquals(this.priceA, this.priceB);
+  }
+
+  @Test
+  void testHashCode() {
+    assertEquals(this.priceA.hashCode(), this.priceC.hashCode());
+    assertNotEquals(this.priceA.hashCode(), this.priceB.hashCode());
+  }
+
+  @Test
+  void testToString() {
+    assertEquals("PriceDto(price=123.0, timestamp=123)", this.priceA.toString());
+  }
+}
---
+++ src/test/java/com/capgemini/fs/coindashboard/apiCommunicator/dtos/marketData/QuoteDtoTest.java
@@ -0,0 +1,45 @@
+package com.capgemini.fs.coindashboard.apiCommunicator.dtos.marketData;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+class QuoteDtoTest {
+
+  private final QuoteDto quoteA = new QuoteDto(new ArrayList<>() {{
+    add(new PriceDto(123d, 123L));
+  }}, new ArrayList<>() {{
+    add(new DeltaDto(IntervalEnum.ONE_HOUR, 123d, 123d));
+  }}, 1233L);
+  private final QuoteDto quoteB = new QuoteDto(new ArrayList<>() {{
+    add(new PriceDto(123d, 123L));
+  }}, new ArrayList<>() {{
+    add(new DeltaDto(IntervalEnum.ONE_HOUR, 123d, 123d));
+  }}, 1233L);
+  private final QuoteDto quoteC = new QuoteDto(new ArrayList<>() {{
+    add(new PriceDto(13d, 123L));
+  }}, new ArrayList<>() {{
+    add(new DeltaDto(IntervalEnum.ONE_HOUR, 123d, 123d));
+  }}, 1233L);
+
+  @Test
+  void testEquals() {
+    assertEquals(this.quoteA, this.quoteB);
+    assertNotEquals(this.quoteA, this.quoteC);
+  }
+
+  @Test
+  void testHashCode() {
+    assertEquals(this.quoteA.hashCode(), this.quoteB.hashCode());
+    assertNotEquals(this.quoteA.hashCode(), this.quoteC.hashCode());
+  }
+
+  @Test
+  void testToString() {
+    assertEquals(
+        "QuoteDto(prices=[PriceDto(price=123.0, timestamp=123)], deltas=[DeltaDto(interval=ONE_HOUR, percentChange=123.0, nominalChange=123.0)], lastUpdateTimestampMillis=1233)",
+        this.quoteB.toString());
+  }
+}
