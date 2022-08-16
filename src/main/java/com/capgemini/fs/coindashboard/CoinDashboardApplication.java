46dc987a3fdb928ba7b377bcd2980954c52a3b76
jszewior <joachim.szewior@capgemini.com>
2022-08-16 13:17:32 +0200

    New sub-task, problems with this.condition=null

--- src/main/java/com/capgemini/fs/coindashboard/CoinDashboardApplication.java
+++ src/main/java/com/capgemini/fs/coindashboard/CoinDashboardApplication.java
@@ -1,9 +1,14 @@
 package com.capgemini.fs.coindashboard;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {
+        "com.capgemini.fs.coindashboard.API_documentation",
+    })
 public class CoinDashboardApplication {
 
   public static void main(String[] args) {
