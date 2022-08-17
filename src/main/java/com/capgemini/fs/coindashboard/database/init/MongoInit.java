94830387d1facf97891cd2e0b7a84ea337a0c3e3
21JFX <jakub.a.adamczyk@capgemini.com>
2022-08-17 14:07:04 +0200

    Mongo Init, changes to make it as a spring boot @Component

--- src/main/java/com/capgemini/fs/coindashboard/database/init/MongoInit.java
+++ src/main/java/com/capgemini/fs/coindashboard/database/init/MongoInit.java
@@ -3,26 +3,30 @@ package com.capgemini.fs.coindashboard.database.init;
 import com.capgemini.fs.coindashboard.apiCommunicator.ApiHolder;
 import com.capgemini.fs.coindashboard.database.queries.CreateQueries;
 import com.capgemini.fs.coindashboard.dtos.marketData.CoinMarketDataResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
 
+@Component
 // class for initialization of a mongoDB with historical data of coins
 public class MongoInit implements CommandLineRunner {
-  @Autowired private CoinMarketDataResult coinMarketDataDto;
-  @Autowired private CreateQueries createQueries;
-  @Autowired ApiHolder apiholder;
-
-  public static void main(String[] args) {
-    SpringApplication.run(MongoInit.class, args);
-  }
-
+  private static final Logger log = LogManager.getLogger(MongoInit.class);
+  @Autowired
+  private CreateQueries createQueries;
+  @Autowired
+  private ApiHolder apiHolder;
   @Override
-  public void run(String... args) throws Exception {
-    coinMarketDataDto = apiholder.getCoinMarketData("btc");
-    // createQueries.CreateCoinDocument(coinMarketDataDto);
+  public void run(String... args) throws Exception{
+    log.info("Starting initialization...");
+    CoinMarketDataResult coinMarketDataResult = apiHolder.getCoinMarketData("btc");
+    if (coinMarketDataResult != null) {
+      createQueries.CreateCoinDocument(coinMarketDataResult.getCoinMarketDataDTOS().get(0));
+    }
+    else{
+      log.info("Data not loaded from the APIHolder");
+    }
   }
 }
