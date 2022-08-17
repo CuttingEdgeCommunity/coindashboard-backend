4ef90db0c5bbae9d551d6537ca78d1f048f6a317
jmakarew <jakub.makarewicz@capgemini.com>
2022-08-17 11:55:15 +0200

    SIB-118 fixed locale formatting

--- src/test/java/com/capgemini/fs/coindashboard/apiCommunicator/utils/TimeFormatterTest.java
+++ src/test/java/com/capgemini/fs/coindashboard/apiCommunicator/utils/TimeFormatterTest.java
@@ -10,7 +10,7 @@ class TimeFormatterTest {
   @Test
   void convertStringToTimestamp() throws ParseException {
     assertEquals(
-        1659674716000L,
+        1659681916000L,
         TimeFormatter.convertStringToTimestamp("08/05/2022 06:45:16", "MM/dd/yyyy hh:mm:ss")
             .getTime());
   }
