689de9ea3fade0e571e5515dd61bcc4ef6d3329c
jmakarew <jakub.makarewicz@capgemini.com>
2022-08-22 11:31:03 +0200

    SIB-118 Testing Disabled unused test

--- src/test/java/com/capgemini/fs/coindashboard/CoinDashboardApplicationTests.java
+++ src/test/java/com/capgemini/fs/coindashboard/CoinDashboardApplicationTests.java
@@ -1,11 +1,14 @@
 package com.capgemini.fs.coindashboard;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class CoinDashboardApplicationTests {
 
   @Test
+  @Disabled
   void contextLoads() {}
 }
