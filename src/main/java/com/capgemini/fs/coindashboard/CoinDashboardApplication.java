f843cfd3fda81fbdf11de0694b57ff95d374e0d5
jszewior <joachim.szewior@capgemini.com>
2022-08-22 10:02:30 +0200

    Branch with working openAPI, but its empty(no controllers)

--- src/main/java/com/capgemini/fs/coindashboard/CoinDashboardApplication.java
+++ src/main/java/com/capgemini/fs/coindashboard/CoinDashboardApplication.java
@@ -1,9 +1,23 @@
 package com.capgemini.fs.coindashboard;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {
+      "com.capgemini.fs.coindashboard.controller",
+      "com.capgemini.fs.coindashboard.database",
+      "com.capgemini.fs.coindashboard.initService",
+      "com.capgemini.fs.coindashboard.CRUDService",
+      "com.capgemini.fs.coindashboard.apiCommunicator",
+      "com.capgemini.fs.coindashboard.updatingService",
+      "com.capgemini.fs.coindashboard.cacheService"
+    })
+@EnableScheduling
+@OpenAPIDefinition(info = @Info(title = "API Documentation",version = "1.1",description = "Finally working"))
 public class CoinDashboardApplication {
 
   public static void main(String[] args) {
