efa1f278eed2a13f33155d0b065f32f4217c3441
Tomasz Zdeb <tomasz.zdeb@capgemini.com>
2022-09-09 11:29:50 +0200

    Added to-do/resource comments

--- src/main/java/com/capgemini/fs/coindashboard/configuration/SecretPropertiesConfiguration.java
+++ src/main/java/com/capgemini/fs/coindashboard/configuration/SecretPropertiesConfiguration.java
@@ -1,5 +1,23 @@
 package com.capgemini.fs.coindashboard.configuration;
 
-public class SecretPropertiesConfiguration {
+import com.capgemini.fs.coindashboard.encryptionService.AESService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.SpringDataMongoDB;
 
+@Configuration
+public class SecretPropertiesConfiguration {
+  private AESService aesService;
+/* chcę wstrzyknąć AESService tutaj i przed zainicjalizowaniem zaszyfrowanej property odszyfrować ją
+
+  /* How to override autoconfiguration in spring boot */
+  /* How to override spring-boot-starter-data-mongodb configuration
+  *
+  * src/main/resources/META-INF/spring.factories
+  *
+  * org.springframework.boot.autoconfigure.EnableAutoConfiguration
+  * io.spring.lab.common.event.DomainEventPublisherAutoConfiguration
+  * io.spring.lab.common.facade.FacadeAutoConfiguration
+  * io.spring.lab.common.math.MathPropertiesAutoConfiguration
+  */
 }
