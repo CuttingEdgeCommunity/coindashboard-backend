6e2b84d92ef9f36461bb2719e6128ecb877cb19a
Tomasz Zdeb <tomasz.zdeb@capgemini.com>
2022-09-15 13:04:54 +0200

    Applied spotless

--- src/main/java/com/capgemini/fs/coindashboard/configuration/SecretsPostProcessor.java
+++ src/main/java/com/capgemini/fs/coindashboard/configuration/SecretsPostProcessor.java
@@ -1,6 +1,12 @@
 package com.capgemini.fs.coindashboard.configuration;
 
 import com.capgemini.fs.coindashboard.encryptionService.AESService;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.boot.logging.DeferredLog;
@@ -11,68 +17,60 @@ import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.stereotype.Component;
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+
 @Component
-public class SecretsPostProcessor implements EnvironmentPostProcessor,
-    ApplicationListener<ApplicationEvent> {
+public class SecretsPostProcessor
+    implements EnvironmentPostProcessor, ApplicationListener<ApplicationEvent> {
 
   private static final DeferredLog log = new DeferredLog();
   private AESService aesService;
 
   @Override
-  public void postProcessEnvironment(ConfigurableEnvironment environment,
-      SpringApplication application) {
+  public void postProcessEnvironment(
+      ConfigurableEnvironment environment, SpringApplication application) {
 
-    //configurationProperties is a main configuration properties source
-    PropertySource<?> propertySource = environment.getPropertySources()
-        .get("configurationProperties");
+    // configurationProperties is a main configuration properties source
+    PropertySource<?> propertySource =
+        environment.getPropertySources().get("configurationProperties");
 
-    if(propertySource.getProperty("secret.key")==null ||
-        propertySource.getProperty("secret.iv") == null){
+    if (propertySource.getProperty("secret.key") == null
+        || propertySource.getProperty("secret.iv") == null) {
       log.error("Can't find secret key and/or iv");
       return;
     }
 
-    aesService = new AESService(propertySource.getProperty("secret.key").toString(),
-        propertySource.getProperty("secret.iv").toString());
+    aesService =
+        new AESService(
+            propertySource.getProperty("secret.key").toString(),
+            propertySource.getProperty("secret.iv").toString());
 
     String secretPropertiesString = propertySource.getProperty("secret.properties").toString();
 
     List<String> secretProperties = Arrays.stream(secretPropertiesString.split(" ")).toList(),
-      propertiesNotFound = new ArrayList<>(),
-      propertiesNotDecrypted = new ArrayList<>();
+        propertiesNotFound = new ArrayList<>(),
+        propertiesNotDecrypted = new ArrayList<>();
 
     StringBuilder decryptedPropertiesStringBuilder = new StringBuilder();
 
     for (String secret : secretProperties) {
-      if(propertySource.getProperty(secret) != null){
-        try{
-          String decryptedSecret = aesService.decrypt(propertySource.getProperty(secret).toString());
-          decryptedPropertiesStringBuilder.append(secret+"="+decryptedSecret+"\n");
-        }
-        catch (Exception ex)
-        {
+      if (propertySource.getProperty(secret) != null) {
+        try {
+          String decryptedSecret =
+              aesService.decrypt(propertySource.getProperty(secret).toString());
+          decryptedPropertiesStringBuilder.append(secret + "=" + decryptedSecret + "\n");
+        } catch (Exception ex) {
           propertiesNotDecrypted.add(secret);
         }
-      }
-      else
-      {
+      } else {
         propertiesNotFound.add(secret);
       }
     }
 
-    if(propertiesNotFound.size()>=1)
-    {
-      logTroubles(propertiesNotFound,"find");
+    if (propertiesNotFound.size() >= 1) {
+      logTroubles(propertiesNotFound, "find");
     }
-    if(propertiesNotDecrypted.size()>=1)
-    {
-      logTroubles(propertiesNotDecrypted,"decrypt");
+    if (propertiesNotDecrypted.size() >= 1) {
+      logTroubles(propertiesNotDecrypted, "decrypt");
     }
 
     String decryptedPropertiesString = decryptedPropertiesStringBuilder.toString();
@@ -80,28 +78,27 @@ public class SecretsPostProcessor implements EnvironmentPostProcessor,
     MutablePropertySources propertySourcesHandle = environment.getPropertySources();
 
     Properties properties = new Properties();
-    try{
+    try {
       properties.load(new StringReader(decryptedPropertiesString));
-    }
-    catch(IOException exception) {
+    } catch (IOException exception) {
       log.warn("Can't fetch decrypted properties into configuration");
     }
-    PropertiesPropertySource decryptedSecretsPropertySource = new PropertiesPropertySource(
-        "decryptedSecretsProperties", properties);
+    PropertiesPropertySource decryptedSecretsPropertySource =
+        new PropertiesPropertySource("decryptedSecretsProperties", properties);
     propertySourcesHandle.addFirst(decryptedSecretsPropertySource);
 
-    if(propertiesNotFound.size() < 1 && propertiesNotDecrypted.size() < 1){
+    if (propertiesNotFound.size() < 1 && propertiesNotDecrypted.size() < 1) {
       log.info("Successfully decrypted all properties");
     }
   }
 
-  private void logTroubles(List<String> problemSource,String actionName){
+  private void logTroubles(List<String> problemSource, String actionName) {
     StringBuilder propertiesNotFoundStringBuilder = new StringBuilder();
     String propertiesWord = problemSource.size() == 1 ? "property" : "properties";
-    propertiesNotFoundStringBuilder.append("Can't "+actionName+" "+
-        problemSource.size()+" "+propertiesWord+": [ ");
-    for (String propertyNotFound: problemSource) {
-      propertiesNotFoundStringBuilder.append(propertyNotFound+" ");
+    propertiesNotFoundStringBuilder.append(
+        "Can't " + actionName + " " + problemSource.size() + " " + propertiesWord + ": [ ");
+    for (String propertyNotFound : problemSource) {
+      propertiesNotFoundStringBuilder.append(propertyNotFound + " ");
     }
     propertiesNotFoundStringBuilder.append("]");
     log.warn(propertiesNotFoundStringBuilder.toString());
--- src/main/java/com/capgemini/fs/coindashboard/encryptionService/AESService.java
+++ src/main/java/com/capgemini/fs/coindashboard/encryptionService/AESService.java
@@ -1,39 +1,36 @@
 package com.capgemini.fs.coindashboard.encryptionService;
 
-import org.springframework.beans.factory.annotation.Value;
-import javax.crypto.*;
-import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
-
+import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class AESService {
   private SecretKey key;
   private final String TRANSFORMATION = "AES/GCM/NoPadding";
   private final int TAG_LENGTH = 128;
   private byte[] IV;
-  public AESService(String secret_key,String iv) {
-    key = new SecretKeySpec(decode(secret_key),"AES");
+
+  public AESService(String secret_key, String iv) {
+    key = new SecretKeySpec(decode(secret_key), "AES");
     this.IV = decode(iv);
   }
 
-  public String decrypt(String encryptedMessage) throws NoSuchPaddingException,
-      NoSuchAlgorithmException,
-      InvalidKeyException,
-      InvalidAlgorithmParameterException,
-      IllegalBlockSizeException,
-      BadPaddingException{
+  public String decrypt(String encryptedMessage)
+      throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException,
+          InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
     byte[] messageInBytes = decode(encryptedMessage);
     Cipher decryptionCipher = Cipher.getInstance(TRANSFORMATION);
-    GCMParameterSpec spec = new GCMParameterSpec(TAG_LENGTH,IV);
-    decryptionCipher.init(Cipher.DECRYPT_MODE,key,spec);
+    GCMParameterSpec spec = new GCMParameterSpec(TAG_LENGTH, IV);
+    decryptionCipher.init(Cipher.DECRYPT_MODE, key, spec);
     byte[] decryptedBytes = decryptionCipher.doFinal(messageInBytes);
     return new String(decryptedBytes);
   }
 
-
-  private byte[] decode(String data){ return Base64.getDecoder().decode(data); }
+  private byte[] decode(String data) {
+    return Base64.getDecoder().decode(data);
+  }
 }
