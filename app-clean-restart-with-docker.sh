ca771cebd132405aac42e7139176ace367b6abe9
Witold Dziadosz <witold.dziadosz@capgemini.com>
2022-09-16 10:01:28 +0200

    add traefik

--- app-clean-restart-with-docker.sh
+++ app-clean-restart-with-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
 ./mvnw clean spotless:apply package -DskipTests && docker compose down --rmi all --volumes && docker compose build --no-cache && docker compose up
 
 # this comment is unimportant
