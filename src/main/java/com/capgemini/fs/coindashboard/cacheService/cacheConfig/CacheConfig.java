b0310f9ab696f3291ef136cf9779b82db920081d
jmakarew <jakub.makarewicz@capgemini.com>
2022-09-20 09:37:45 +0200

    SIB-142 removed hardcoded integers - Moved cache properties to CacheEnv and cache.properties

--- src/main/java/com/capgemini/fs/coindashboard/cacheService/cacheConfig/CacheConfig.java
+++ src/main/java/com/capgemini/fs/coindashboard/cacheService/cacheConfig/CacheConfig.java
@@ -2,6 +2,7 @@ package com.capgemini.fs.coindashboard.cacheService.cacheConfig;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableCaching
 public class CacheConfig {
+  @Autowired private CacheEnv cacheEnv;
 
   @Bean
   public CacheManager cacheManager() {
@@ -19,32 +21,36 @@ public class CacheConfig {
     caffeineCacheManager.registerCustomCache(
         "getCoinMarketData",
         Caffeine.newBuilder()
-            .maximumSize(1000)
-            .expireAfterWrite(6000, TimeUnit.MILLISECONDS)
+            .maximumSize(cacheEnv.getMax_size_get_coin_market_data())
+            .expireAfterWrite(
+                cacheEnv.getExpire_milliseconds_get_coin_market_data(), TimeUnit.MILLISECONDS)
             .build());
     caffeineCacheManager.registerCustomCache(
         "getCoinInfo",
         Caffeine.newBuilder()
-            .maximumSize(2000)
-            .expireAfterWrite(12000, TimeUnit.MILLISECONDS)
+            .maximumSize(cacheEnv.getMax_size_get_coin_info())
+            .expireAfterWrite(
+                cacheEnv.getExpire_milliseconds_get_coin_info(), TimeUnit.MILLISECONDS)
             .build());
     caffeineCacheManager.registerCustomCache(
         "getCoinDetails",
         Caffeine.newBuilder()
-            .maximumSize(1000)
-            .expireAfterWrite(6000, TimeUnit.MILLISECONDS)
+            .maximumSize(cacheEnv.getMax_size_get_coin_details())
+            .expireAfterWrite(
+                cacheEnv.getExpire_milliseconds_get_coin_details(), TimeUnit.MILLISECONDS)
             .build());
     caffeineCacheManager.registerCustomCache(
         "getChart",
         Caffeine.newBuilder()
-            .maximumSize(2000)
-            .expireAfterWrite(1200, TimeUnit.MILLISECONDS)
+            .maximumSize(cacheEnv.getMax_size_get_chart())
+            .expireAfterWrite(cacheEnv.getExpire_milliseconds_get_chart(), TimeUnit.MILLISECONDS)
             .build());
     caffeineCacheManager.registerCustomCache(
         "getCoinByRegex",
         Caffeine.newBuilder()
-            .maximumSize(2000)
-            .expireAfterWrite(6000, TimeUnit.MILLISECONDS)
+            .maximumSize(cacheEnv.getMax_size_get_coin_by_regex())
+            .expireAfterWrite(
+                cacheEnv.getExpire_milliseconds_get_coin_by_regex(), TimeUnit.MILLISECONDS)
             .build());
     return caffeineCacheManager;
   }
